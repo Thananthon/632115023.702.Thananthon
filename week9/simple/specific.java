e13e0e81e30045c55aa673eed6d224d201b2f2d0
Thananthon <tammtananton@gmail.com>
2021-03-04 00:57:30 +0700

    week9

---
+++ week9/simple/specific.java
@@ -0,0 +1,17 @@
+package simple;
+public class specific extends Person{
+    String gender;
+    String singerMood;
+
+    public specific(String name, int bornYear, String gender, String singerMood){
+        super(name,bornYear);
+        this.gender = gender;
+        this.singerMood = singerMood;
+    }
+
+    public void introduce(){
+        super.introduce();
+        System.out.println("Gender: "+ gender +", "+ name + " is a "+ singerMood + " super star who is very popular in " + (bornYear+25) +".");
+        
+    }
+}
